3c5701fa14bce1fcb64b393dfc97c9a755ea5351
chantiqavanessia <chantiqavanessia@students.undip.ac.id>
2026-05-31 21:49:07 +0700

    Fix formatting in useEffect cleanup function

--- src/App.js
+++ src/App.js
@@ -157,7 +157,7 @@ function SplashScreen({ onDone }) {
     const t = setTimeout(onDone, 2200);
     return () => clearTimeout(t);
 // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []); 711d5cac5ebc7be732c8676261d7c9125b4f9a89
+  }, []); 
   return (
     <div style={{
       height: "100%", display: "flex", flexDirection: "column",
